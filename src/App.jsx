76a4826cec76b2bb95eb104b7c906fe5a09816b1
Dimas Bhagaskoro <bhgskr89@gmail.com>
2026-03-31 09:05:00 +0700

    variant class tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,58 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import reactLogo from './assets/react.svg'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const courses = [
+    { title: "System Administration and IT Infrastructure Services" },
+    { title: "Operating Systems Becoming a Power User" },
+    { title: "The Bits and Bytes of Computer Networking" },
+    { title: "Technical Support Fundamentals" },
+    { title: "How to Succeed at: Writing Applications" },
+    { title: "Medicine Administration for Carers" },
+  ];
 
   return (
-    <div className="min-h-screen w-full bg-neutral-800 flex flex-col items-center justify-center text-white">
-      <div className="flex items-center justify-center space-x-8 mb-12">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="w-24 h-24 hover:scale-110 transition" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+    <div className="p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      {courses.map((course, index) => (
+        <div key={index} className="shadow rounded-lg overflow-hidden bg-white">
           <img
-            src={reactLogo}
-            className="w-24 h-24 animate-spin hover:scale-110 transition"
-            alt="React logo"
-            style={{ animationDuration: '10s' }}
+            src="https://www.placehold.co/300x200"
+            alt={course.title}
+            className="w-full"
           />
-        </a>
-      </div>
 
-      <h1 className="text-white text-6xl font-bold mb-16">Vite + React</h1>
+          <div className="p-4 bg-red-100">
+            <h3 className="text-lg font-semibold mb-4">{course.title}</h3>
 
-      <div className="text-sm space-y-6 mb-8 text-center">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="bg-neutral-900 hover:bg-neutral-700 transition text-white py-2 px-6 rounded-lg text-lg shadow-lg"
-        >
-          count is {count}
-        </button>
+            <div className="bg-red-50 p-3 rounded-lg mb-4">
+              <div className="text-sm text-gray-500 flex justify-between mt-2">
+                <span>👥 123 users</span>
+                <span>⏱ 60 min</span>
+              </div>
 
-        <p className="text-neutral-400">
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+              <div className="mt-3 mb-2 flex items-center gap-2">
+                <img
+                  src="https://www.placehold.co/50x50"
+                  alt="Author's Avatar"
+                  className="w-12 h-12 rounded-full object-cover"
+                />
+                <div>
+                  <p className="text-sm font-medium">Author's Name</p>
+                  <p className="text-xs text-gray-500">Designer</p>
+                </div>
+              </div>
+            </div>
 
-      <p className="text-neutral-500">
-        Click on the Vite and React logos to learn more
-      </p>
+            <div className="mt-4 flex items-center justify-between">
+              <div className="bg-white py-2 px-4 rounded">$123</div>
+              <button className="bg-red-700 hover:bg-red-900 active:bg-red-500 text-white px-4 py-2 rounded text-sm">
+                Get Started
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
