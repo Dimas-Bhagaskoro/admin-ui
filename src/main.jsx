f460a0433d005bea61e5246304eb25975f414ac6
Dimas Bhagaskoro <bhgskr89@gmail.com>
2026-04-21 11:00:37 +0700

    benerin filename Exercise

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Exercise from './assets/exercise.jsx'
+import Exercise from './assets/Exercise.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
