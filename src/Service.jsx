c4087f4a8e7ab88c5a5a1cb71417c1b483d4e1d7
Dimas Bhagaskoro <bhgskr89@gmail.com>
2026-04-26 17:17:40 +0700

    benerin struktur api

--- src/Service.jsx
+++ src/Service.jsx
@@ -21,16 +21,18 @@
     },
   ];
 
-  export const getUsers = async () => {
+ export const getUsers = async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await response.json();
+
     return users.map((user) => ({
       name: user.name,
       email: user.email,
-      city: user.city,
-      street: user.street,
+      city: user.address.city,
+      street: user.address.street,
     }));
+
   } catch (error) {
     console.error("[Services] Gagal mengambil data:", error.message);
     throw error;
